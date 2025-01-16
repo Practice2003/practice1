b0883c7f584fa9f5fa7ab8c21800f91c4067126e
Shirsath <I45317@verisk.com>
2025-01-16 10:20:06 +0530

    first_commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+namespace Name
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            List<int> l = new List<int>{1,2,3,4,5};
+            var x = l.Where(n=>n>2);
+            foreach(var s1 in x){
+              Console.WriteLine(s1);
+            }
+        }
+    }
+
+
+
+
+}
+
